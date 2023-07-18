45f4a079d53f2d2d8e74ad142810f32a0c6a009d
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2023-07-18 07:57:52 +0100

    including Andrew's tests

---
+++ tests/testthat/test-projector.R
@@ -0,0 +1,31 @@
+test_that("projection using scenario A returns I.mid of 290.58552", {
+  # Arrange
+  
+  data <- jsonlite::fromJSON('../../inst/plumber/tbstatisticalserver/projection.json')
+  
+  set.seed(123)
+
+  # Act
+  
+  result <- fort::projections(
+    data$year,
+    data$iHat,
+    data$sEi,
+    data$nHat,
+    data$sEn,
+    data$mHat,
+    data$sEm,
+    TXf = 0,
+    HRd = 1,
+    HRi = 1,
+    ORt = 1
+  )
+  
+  # Assert
+  
+  print(names(result$I.mid))
+  print(result$I.mid[1])
+  print(result$I.mid[2])
+  
+  testthat::expect_equal(result$I.mid[1], 290.58552)
+})
